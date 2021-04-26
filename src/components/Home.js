048871d681223857b9550ab101adaa9e5a6e2ac7
ashishsurya126 <ashishsurya126@gmail.com>
2021-04-27 00:31:49 +0530

    completed cpmany logos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import './Home.css';
 
-const iconUrls = [
-  {
-    id: 1,
-    url: 'http://madeuptheatre.com/wp-content/uploads/2019/09/73970c892d748c7507db8e10d71535b0-apple-logo-icon-by-vexels.png',
-  },
-  {
-    id: 2,
-    url:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png',
-  },
-  { id: 3, url: '' },
-  { id: 4, url: '' },
-  { id: 5, url: '' },
-];
+const names = ['Apple', 'Google', 'Microsoft', 'Amazon', 'Spotify'];
 
 const Home = () => {
   return (
@@ -22,25 +9,29 @@ const Home = () => {
       <div className='home__Ad'>
         Learn to code - for free.
         <br />
+        <br />
         Build projects.
+        <br />
         <br />
         Earn certifications.
       </div>
       <div className='home__alumni'>
         <article>
           Since 2014, more than 40,000 freeCodeCamp.org graduates have gotten
-          jobs at tech companies including
+          jobs at tech companies including:
         </article>
         <div
-          style={{ display: 'flex', alignItems: 'center', justifyContent:'center' }}
-          className='home__comapanyLogos'
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginTop: '40px',
+            padding: '2px',
+          }}
+          className='home__comapanyNames'
         >
-          {iconUrls.map(({ url }) => (
-            <img
-              style={{ width: 100, objectFit: 'contain' }}
-              src={url}
-              alt=''
-            />
+          {names.map((name) => (
+            <section className='home__companyName'>{name}</section>
           ))}
         </div>
       </div>
